e5305cf80196836bac709db9a8fbe86de774987e
Alankritha-24 <54307560+Alankritha-24@users.noreply.github.com>
2020-09-08 11:48:46 +0530

    Create step_response_a1.py

---
+++ step_response_a1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+num = np.poly1d([1,0])
+den = np.poly1d([1,3,2,1])
+sys = signal.TransferFunction(num,den)
+t,y = signal.step(sys)
+plt.plot(t,y)
+plt.xlabel('$t$')
+plt.ylabel('$y$')
+plt.grid() # minor
+plt.axis()
+plt.show()
